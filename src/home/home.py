d968bc5c423b2a090a2ac4d05ed4cd9646c75ff6
Ella <ellamadelineberg@gmail.com>
2022-01-14 21:36:10 +0200

    Turn query strings to lower case

--- src/home/home.py
+++ src/home/home.py
@@ -16,8 +16,8 @@ def home():
 
 @home_bp.route("/result")
 def result():
-    query = request.args["query"]
-    sql = "SELECT name, description, address FROM restaurant WHERE address LIKE :query" # todo: modify to use lower case
+    query = request.args["query"].lower()
+    sql = "SELECT name, description, address FROM restaurant WHERE lower(address) LIKE :query" # todo: modify to use lower case
     result = db.session.execute(sql, {"query":"%"+query+"%"})
     restaurants = result.fetchall()
     return render_template(
