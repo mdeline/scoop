2e3f8e013c9d76d9e8b4063743939ca0019daced
Ella <ellamadelineberg@gmail.com>
2022-01-18 20:55:56 +0200

    Transfer restaurants and cafes from Helsinki Open Api to Scoop DB

---
+++ populate_db.py
@@ -0,0 +1,63 @@
+import requests, json
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+            user="",
+            password="",
+            host="localhost",
+            port="5432",
+            database="scoop"
+    )
+except:
+    print("Database connection failed.")
+
+cursor = conn.cursor()
+helsinki_places_json = requests.get('http://open-api.myhelsinki.fi/v2/places/').json()
+data = helsinki_places_json["data"]
+
+for place in data:
+    insert_query = "insert into scoop.restaurant(id, name, info_url, description, street_address, postal_code, city, neighbourhood) values (%s, %s, %s, %s, %s, %s, %s, %s)"
+    if place["name"]["fi"] == None:
+        continue
+
+    is_restaurant_or_cafe = False
+
+    for tag in place["tags"]:
+        tag_name = tag["name"].lower()
+        if tag_name == "restaurants" or tag_name == "cafes" or tag_name == "islandrestaurant" or tag_name == "restaurant" or tag_name == "cafe" or tag_name == "cafesummer" or tag_name == "restaurantsummer":
+            is_restaurant_or_cafe = True
+            continue
+
+    if is_restaurant_or_cafe == False:
+        continue
+
+    cursor.execute(
+        insert_query,
+        (
+            place["id"],
+            place["name"]["fi"],
+            place["info_url"],
+            place["description"]["intro"],
+            place["location"]["address"]["street_address"],
+            place["location"]["address"]["postal_code"],
+            place["location"]["address"]["locality"],
+            place["location"]["address"]["neighbourhood"],
+        )
+    )
+    conn.commit()
+conn.close()
+
+"""helsinki_places_json = requests.get('http://open-api.myhelsinki.fi/v2/places/').json()
+data = helsinki_places_json["data"]
+for place in data:
+    print(place["name"]["en"])
+    print(place["info_url"])
+    print(place["description"]["intro"])
+    print(place["location"]["address"]["street_address"])
+    print(place["location"]["address"]["postal_code"])
+    print(place["location"]["address"]["locality"])
+    print(place["location"]["address"]["neighbourhood"])
+
+for data in helsinki_places_json:
+    print(data)"""
