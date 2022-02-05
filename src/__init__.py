684220296542a5e11498c346fd83b7e74fa770bd
Ella <ellamadelineberg@gmail.com>
2022-02-05 12:01:20 +0200

    Route login, logout and register through auth

--- src/__init__.py
+++ src/__init__.py
@@ -23,7 +23,7 @@ def create_app():
         # Register Blueprints
         app.register_blueprint(home.home_bp)
         app.register_blueprint(venue.venue_bp, url_prefix='/venue')
-        app.register_blueprint(auth.auth_bp)
+        app.register_blueprint(auth.auth_bp, url_prefix='/auth')
         app.register_blueprint(discover.discover_pb, url_prefix='/discover')
 
         return app
