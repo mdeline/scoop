1f5dea1c69c340da5660a69c7cd8fff04a29cbd8
Ella <ellamadelineberg@gmail.com>
2022-03-05 10:47:05 +0200

    Fix img query, use multiline python on sql queries

--- src/discover/discover.py
+++ src/discover/discover.py
@@ -9,24 +9,36 @@ discover_pb = Blueprint(
 
 @discover_pb.route('/', methods=['GET'])
 def discover_all():
-    neighbourhoods = db.session.execute(
-        'with neighbourhood_images as ('
-        + 'select neighbourhood, img_url, '
-        + 'row_number() over ('
-        + 'partition by neighbourhood order by random() '
-        + ') as row_num '
-        + 'from venue where neighbourhood is not null '
-        + 'and img_url is not null), '
-        + 'neighbourhood_venues as ('
-        + 'select neighbourhood, count(*) as venues_count '
-        + 'from venue '
-        + 'group by neighbourhood) '
-        + 'select i.neighbourhood as name, i.img_url, v.venues_count '
-        + 'from neighbourhood_images i '
-        + 'inner join neighbourhood_venues v on v.neighbourhood = i.neighbourhood '
-        + 'where i.row_num = 1 '
-        + 'order by v.venues_count desc'
-    ).fetchall()
+    sql = '''
+        with neighbourhood_images as (
+	        select 
+		        neighbourhood_id, 
+		        img_url, 
+    	        row_number() over (
+    		        partition by neighbourhood order by random()
+    	        ) as row_num
+            from scoop.venue 
+	        where neighbourhood_id is not null
+            and img_url is not null
+        ),
+        neighbourhood_venues as (
+	        select 
+		        neighbourhood_id, 
+		        count(*) as venues_count
+            from scoop.venue
+            group by neighbourhood_id
+        )
+        select 
+	        nh.name, 
+	        images.img_url, 
+	        venues.venues_count
+            from neighbourhood_images images
+            inner join neighbourhood_venues venues on venues.neighbourhood_id = images.neighbourhood_id
+            inner join scoop.neighbourhood nh on nh.id = images.neighbourhood_id
+            where images.row_num = 1
+            order by venues.venues_count desc;
+    '''
+    neighbourhoods = db.session.execute(sql).fetchall()
 
     return render_template(
         'discover.jinja2',
@@ -38,23 +50,23 @@ def discover_query():
     query = request.args["query"]
     query_cleaned = query.lower()
 
-    sql_results = (
-        "select * from venue "
-        + "where lower(name) like :query_cleaned "
-        + "or lower(street_address) like :query_cleaned "
-        + "or postal_code like :query_cleaned "
-        + "or lower(city) like :query_cleaned " 
-        + "or lower(neighbourhood) like :query_cleaned"
-    )
+    sql_results = '''
+        select * from venue
+        where lower(name) like :query_cleaned
+        or lower(street_address) like :query_cleaned
+        or postal_code like :query_cleaned
+        or lower(city) like :query_cleaned 
+        or lower(neighbourhood) like :query_cleaned;
+    '''
 
-    sql_aggregates = (
-        "select count(*) as count from venue "
-        + "where lower(name) like :query_cleaned "
-        + "or lower(street_address) like :query_cleaned "
-        + "or postal_code like :query_cleaned "
-        + "or lower(city) like :query_cleaned " 
-        + "or lower(neighbourhood) like :query_cleaned"
-    )
+    sql_aggregates = '''
+        select count(*) as count from venue
+        where lower(name) like :query_cleaned
+        or lower(street_address) like :query_cleaned
+        or postal_code like :query_cleaned
+        or lower(city) like :query_cleaned 
+        or lower(neighbourhood) like :query_cleaned;
+    '''
 
     venues = db.session.execute(
         sql_results, 
@@ -78,12 +90,12 @@ def discover_neighbourhood():
     query = request.args["query"]
 
     venues = db.session.execute(
-        "select * from venue where neighbourhood=:query", 
+        "select * from venue where neighbourhood=:query;", 
         {"query":query}
     ).fetchall()
 
     venue_aggregates = db.session.execute(
-        "select count(*) as count from venue where neighbourhood=:query",
+        "select count(*) as count from venue where neighbourhood=:query;",
         {"query":query}
     ).fetchone()
 
