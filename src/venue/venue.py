2d7ecc21c2f4e40b0897f56aa9b1077c4e49866a
Ella <ellamadelineberg@gmail.com>
2022-03-06 11:12:31 +0200

    Display error message if review form is not valid

--- src/venue/venue.py
+++ src/venue/venue.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import flash, Blueprint, render_template, request, redirect, url_for, session
 from .. import db
 
 venue_bp = Blueprint(
@@ -9,33 +9,45 @@ venue_bp = Blueprint(
 
 @venue_bp.route('/<venue_id>', methods=['GET'])
 def venue(venue_id):
+    # Last venue page visited
+    session["recent_venue"] = venue_id
+
     # Venue info
     venue = db.session.execute(
-        'select * from scoop.venue '
-        + 'where id = :venue_id',
+        'select * from scoop.venue where id = :venue_id',
         {'venue_id': venue_id}
     ).fetchone()
 
     # Review count & review average
-    review_aggregates = db.session.execute(
-        'select '
-        + 'coalesce(avg(stars)::numeric(3,2), 0) as review_avg, '
-        + 'coalesce(count(review), 0) as review_count '
-        + 'from scoop.review '
-        + 'where venue_id = :venue_id '
-        + 'and deleted = false',
+    review_aggregates = db.session.execute('''
+        select 
+            coalesce(avg(stars)::numeric(3,2), 0) as review_avg,
+            coalesce(count(review), 0) as review_count
+        from scoop.review
+        where venue_id = :venue_id
+        and deleted = false;
+        ''',
         {'venue_id': venue_id}
     ).fetchone()
 
     # Reviews
-    reviews = db.session.execute(
-        'select review.id, review, stars, appuser.fullname as user, '
-        + 'appuser_id, created_at, modified_at, img_url as user_photo from review '
-        + 'inner join scoop.appuser on scoop.appuser.id = scoop.review.appuser_id '
-        + 'where venue_id = :venue_id '
-        + 'and deleted = false '
-        + 'order by created_at desc',
-        {'venue_id':venue_id}
+    reviews = db.session.execute('''
+        select 
+            review.id, 
+            review, 
+            stars, 
+            appuser.fullname as user,
+            appuser_id, 
+            created_at, 
+            modified_at, 
+            img_url as user_photo 
+        from review
+        inner join scoop.appuser on scoop.appuser.id = scoop.review.appuser_id
+        where venue_id = :venue_id
+        and deleted = false
+        order by created_at desc
+    ''',
+    {'venue_id':venue_id}
     ).fetchall()
 
     if venue:
@@ -45,24 +57,30 @@ def venue(venue_id):
             review_aggregates=review_aggregates,
             reviews=reviews
         )
-    return redirect(url_for("home_bp.home")) 
-
+    else:
+        return redirect(url_for("home_bp.home"))
+        
 @venue_bp.route('/review', methods=['POST'])
 def review():
-    review = request.form["review"]
-    rating = request.form["rating"]
-    appuser_id = session["appuser_id"]
-    venue_id = request.form["venue_id"]
-    
-    #todo: use form validation
-    sql = 'insert into scoop.review(review, stars, appuser_id, venue_id, deleted) values(:review, :rating, :appuser_id, :venue_id, false)'
-    db.session.execute(sql, {
-        'review': review,
-        'rating': rating,
-        'appuser_id': appuser_id,
-        'venue_id': venue_id,  
-    })
-    db.session.commit()
+    review = request.form.get('review')
+    rating = request.form.get('rating')
+    venue_id = request.form.get('venue_id')
+    appuser_id = session.get('appuser_id')
+
+    if review and rating and appuser_id:
+        sql = '''
+            insert into scoop.review(review, stars, appuser_id, venue_id, deleted) 
+            values(:review, :rating, :appuser_id, :venue_id, false);
+        '''
+        db.session.execute(sql, {
+            'review': review,
+            'rating': rating,
+            'appuser_id': appuser_id,
+            'venue_id': venue_id,  
+        })
+        db.session.commit()
+    else:
+        flash('Please fill both review and rating fields.')
     return redirect(url_for("venue_bp.venue", venue_id=venue_id))
 
 @venue_bp.route('/<venue_id>/review/<review_id>', methods=['GET'])
