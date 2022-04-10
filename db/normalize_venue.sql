b0ae61050049f4ce5dc0ad7522c04056b6eac139
Ella <ellamadelineberg@gmail.com>
2022-04-10 14:20:54 +0300

    Simplify inserting

--- db/normalize_venue.sql
+++ db/normalize_venue.sql
@@ -1,26 +1,14 @@
--- 1. Populoidaan taulu
-do $$
-	declare 
-		row record;
-	begin
-		for row in (
-			select distinct neighbourhood
-			from scoop.venue
-			where neighbourhood != ''
-		)
-		loop
-			insert into scoop.neighbourhood(name) values(row.neighbourhood);
-			raise notice '%', row.neighbourhood;
-		end loop;
-	end
-$$;
+-- 1. Populate the table 'scoop.neighbourhood' with neighbourhood names
+insert into scoop.neighbourhood(name, created_at)
+select distinct neighbourhood, now() 
+from scoop.venue where neighbourhood != ''
+returning *; 
 
--- 2. Lisätään viiteavain
-alter table scoop.venue add column  neighbourhood_id int null;
-alter table scoop.venue add constraint neighbourhood_fk 
-foreign key (neighbourhood_id) references scoop.neighbourhood(id);
+-- 2. Add a foreign key 'neighbourhood_id' to 'scoop.venue'
+alter table scoop.venue add column neighbourhood_id int null;
+alter table scoop.venue add constraint neighbourhood_fk foreign key (neighbourhood_id) references scoop.neighbourhood(id);
 
--- 3. Populoidaan viiteavain
+-- 3. Populate the foreign key 'neighbourhood_id'
 do $$
 	declare 
 		row record;
@@ -30,7 +18,6 @@ do $$
 				venue.id as venue_id, 
 				venue.name as venue_name,
 				venue.neighbourhood as venue_nh,
-				
 				nh.name as neighbourhood_name,
 				nh.id as neighbourhood_id
 			from scoop.venue
@@ -43,6 +30,6 @@ do $$
 	end
 $$;
 
--- Tarkistusta 
+-- Checking the data 
 select venue.neighbourhood, venue.neighbourhood_id, nh.id, nh.name
 from scoop.venue left join scoop.neighbourhood nh on nh.id = venue.neighbourhood_id
